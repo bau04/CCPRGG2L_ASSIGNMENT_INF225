db828df1cf9a070387eb348d78c182fe919039b6
bau04 <120693660+bau04@users.noreply.github.com>
2023-02-10 11:42:58 +0800

    Create Dinosaur.java

---
+++ Exercise_6.java/Dinosaur.java
@@ -0,0 +1,12 @@
+public class Dinosaur implements Animal {
+    
+public void crawl(){
+    System.out.println("Dinosaur is crawling");
+}
+
+public void run(){
+    System.out.println("Dinosaur is running");
+}
+
+
+}
